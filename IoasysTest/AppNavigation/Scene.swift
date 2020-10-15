f535ad74d25934e8fdd6ea19391b41319743ed58
Gustavo Ludtke <gustavoludtke@ioasys.com.br>
2020-10-15 12:47:35 -0300

    App Coordinator added and configured

---
+++ IoasysTest/AppNavigation/Scene.swift
@@ -0,0 +1,15 @@
+//
+//  Scene.swift
+//  AppNavigation
+//
+//  Created by Gustavo Ludtke on 15/10/20.
+//  Copyright © 2020 Gustavo Ludtke. All rights reserved.
+//
+
+import Foundation
+
+public enum Scene {
+    case login
+    case companyList
+    case companyDetail
+}
---
+++ IoasysTest/Features/Common/Coordinator/Coordinator.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinator.swift
+//  AppNavigation
+//
+//  Created by Gustavo Ludtke on 15/10/20.
+//  Copyright © 2020 Gustavo Ludtke. All rights reserved.
+//
+
+import Foundation
+
+public protocol Coordinator {
+    func start()
+}
+
